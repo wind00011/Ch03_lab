4fefee2482350e769b5ed7784111773ec84faf52
wind00011 <113489040+wind00011@users.noreply.github.com>
2023-10-08 22:42:04 +0800

    Add files via upload

---
+++ 3-2-6ex02.sql
@@ -0,0 +1,3 @@
+INSERT INTO Comp_dep
+VALUES ('D003', '祇场')
+--ぃ穦闽羛场絏(龄)把σ闽羛场絏(龄)
---
+++ 3-3-1_query.sql
@@ -0,0 +1,2 @@
+SELECT A.Em_id, Em_name, Q1, Q2 from employee2 as A,Performance as B
+WHERE A.Em_id = B.Em_id
